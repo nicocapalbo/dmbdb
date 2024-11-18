e36f32aebdaca176fb5b8efb049aeea6161fd691
nicocapalbo <nicolas.capalbo@lystos.com>
2024-11-19 00:03:33 +0100

    feat(): material icons

--- components/ServicePoller.vue
+++ components/ServicePoller.vue
@@ -4,6 +4,7 @@ import {computed, onMounted, ref} from "vue";
 
 const {statusService} = useService()
 const data = ref(null)
+const loading = ref(false)
 
 const statusStyle = computed(() => {
   const style = {
@@ -26,9 +27,11 @@ const statusStyle = computed(() => {
 })
 
 const refreshStatus = async () => {
+  loading.value = true
   try {
     const response = await statusService.getHealthCheck()
     data.value = await response.json()
+    loading.value = false
   } catch (e) {
     throw new Error(e)
   }
@@ -42,11 +45,20 @@ onMounted(() => {
 
 <template>
 <div class="w-full flex items-center justify-end gap-4 px-4">
-  <div v-if="data" class="rounded-lg p-2 flex gap-2 items-center" :class="statusStyle.bgColor">
-    <p>DMB Status: <span class="font-medium" :class="statusStyle.textColor">{{data?.status}}</span></p>
-    <div class="h-3 w-3 rounded-full" :class="statusStyle.ballColor" />
+  <div class="relative group">
+    <div v-if="data" class="rounded-lg px-3 py-2 flex gap-2 items-center" :class="statusStyle.bgColor">
+      <p class="text-slate-900">DMB Status:</p>
+      <span v-if="loading" class="animate-spin material-symbols-rounded text-black">progress_activity</span>
+      <div v-else class="flex items-center gap-1">
+        <span class="font-medium" :class="statusStyle.textColor">{{data?.status}}</span>
+        <div class="h-3 w-3 rounded-full" :class="statusStyle.ballColor" />
+      </div>
+    </div>
+    <button class="hidden group-hover:flex items-center justify-center absolute top-0 right-0 bottom-0 left-0 w-full h-full rounded-lg bg-black bg-opacity-40" @click="refreshStatus">
+      <span v-if="loading" class="animate-spin material-symbols-rounded text-white">progress_activity</span>
+      <span v-else class="material-symbols-rounded text-white">refresh</span>
+    </button>
   </div>
-  <button class="bg-blue-500 text-white font-semibold p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-offset" @click="refreshStatus">Refresh status</button>
 </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@ import ServicePoller from "~/components/ServicePoller.vue";
 
 <template>
   <div class="relative p-4 w-full bg-opacity-10 bg-cover bg-center">
-    <img :src="dmbImage" class="-z-10 w-full h-full absolute top-0 right-0 left-0 bottom-0 object-cover opacity-50" alt="">
+    <img :src="dmbImage" class="-z-10 w-full h-full absolute top-0 right-0 left-0 bottom-0 object-cover opacity-20" alt="">
     <ServicePoller />
     <div class="flex flex-col gap-8 w-max mx-auto">
       <ServiceCard
