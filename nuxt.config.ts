df9f6e9031460b71bea4ffbab71c7c4c04f50c07
nicocapalbo <nicolas.capalbo@lystos.com>
2024-11-19 16:39:00 +0100

    feat(): changed dev server port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devServer: {
+    port: 3005
+  },
   ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
