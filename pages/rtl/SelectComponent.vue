d9ec19478b89e5502fe8696a7b38e83cad576fb6
nicocapalbo <nicolas.capalbo@lystos.com>
2025-03-14 23:48:07 +0100

    refactor: minor modifications

---
+++ pages/rtl/SelectComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  items: { type: Array, required: true}
+})
+const itemsList = ref(props.items||[])
+
+const selectedFilter = defineModel()
+</script>
+
+<template>
+  <select id="logLevel" v-model="selectedFilter" class="h-[30px] text-sm bg-transparent text-slate-200 rounded px-2 py-1 border border-slate-600 focus:border-blue-500 outline-none">
+    <option v-for="item in itemsList" :key="item.value" value="">
+      {{item.label}}
+    </option>
+<!--    <option v-for="service in SERVICES" :key="service.process_name" :value="service.process_name">-->
+<!--      {{ service.process_name }}-->
+<!--    </option>-->
+  </select>
+</template>
+
