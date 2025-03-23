f2bba30330ed066339eab7d57e5935b1d0214e88
nicocapalbo <nicolas.capalbo@lystos.com>
2025-03-23 12:47:32 +0100

    style: SideBar component WIP

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -7,7 +7,7 @@ const toggleLogsDropdown = () => { logsDropdown.value = !logsDropdown.value }
 </script>
 
 <template>
-  <div class="z-10 absolute top-0 bottom-0 left-0 md:relative w-full max-w-[200px] flex-shrink-0 border-r border-slate-800 shadow bg-gray-900 text-white overflow-y-auto pb-20 md:pb-0">
+  <div class="z-10 absolute top-0 bottom-0 left-0 md:relative w-full max-w-[250px] flex-shrink-0 border-r border-slate-800 shadow bg-gray-900 text-white overflow-y-auto pb-20 md:pb-0">
     <div class="flex flex-col gap-4 px-2 py-4">
       <NuxtLink :to="{ name: 'index' }" class="px-4 py-2 hover:bg-slate-800 rounded-lg text-3xl" @click="$grid.mobile && toggleSideBar(false)">
         DMB
@@ -15,7 +15,10 @@ const toggleLogsDropdown = () => { logsDropdown.value = !logsDropdown.value }
 
       <div>
         <button class="w-full flex items-center justify-between rounded-lg group hover:bg-gray-800 px-2 py-1" @click="toggleServicesDropdown">
-          <span class="text-lg font-bold">Services</span>
+              <span class="flex items-center gap-2 grow">
+                <span class="material-symbols-rounded !text-[18px]">stacks</span>
+                <span class="text-lg font-bold">Services</span>
+              </span>
           <span :class="[ servicesDropdown ? 'rotate-180' : 'rotate-0' ]" class="material-symbols-rounded group-hover:scale-105 transform transition ease-in-out">expand_more</span>
         </button>
         <div v-if="servicesDropdown" class="px-2">
@@ -26,8 +29,11 @@ const toggleLogsDropdown = () => { logsDropdown.value = !logsDropdown.value }
       </div>
 
       <div>
-        <button class="w-full flex items-center justify-between rounded-lg group hover:bg-gray-800 px-2 py-1" @click="toggleLogsDropdown">
-          <span class="text-lg font-bold">Realtime Logs</span>
+        <button class="w-full button-small group hover:bg-gray-800 px-2 py-1" @click="toggleLogsDropdown">
+              <span class="flex items-center gap-2 grow">
+                <span class="material-symbols-rounded !text-[18px]">data_object</span>
+                <span class="text-lg font-bold">Realtime Logs</span>
+              </span>
           <span :class="[ logsDropdown ? 'rotate-180' : 'rotate-0' ]" class="material-symbols-rounded group-hover:scale-105 transform transition ease-in-out">expand_more</span>
         </button>
         <div v-if="logsDropdown" class="px-2">
