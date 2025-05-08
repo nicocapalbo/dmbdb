532bc92b0f0fe8e70e4b9f23575f10ad3da18ba9
nicocapalbo <nicolas.capalbo@lystos.com>
2025-05-09 00:43:15 +0200

    feat: added enums of services + removed unused constants.js file

--- constants/enums.js
+++ constants/enums.js
@@ -1,3 +1,19 @@
+export const SERVICE_KEY = {
+  DMB_API: "dmb_api_service",
+  DMB_FE: "dmb_frontend",
+  CLI_DEBRID: "cli_debrid",
+  CLI_BATTERY: "cli_battery",
+  PHALANX_DB: "phalanx_db",
+  PLEX_DEBRID: "plex_debrid",
+  POSTGRESS: "postgres",
+  PGADMIN: "pgadmin",
+  RCLONE: "rclone",
+  RIVEN_BE: "riven_backend",
+  RIVEN_FE: "riven_frontend",
+  ZILEAN: "zilean",
+  ZURG: "zurg"
+}
+
 export const SERVICE_ACTIONS = {
   START: 1,
   STOP: 2,
