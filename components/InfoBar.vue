772e6a3f829a5ae62d41984c9ee969aa8f3c57f6
I-am-PUID-0 <36779668+I-am-PUID-0@users.noreply.github.com>
2025-06-30 16:40:28 +0000

    fix: Removes the VersionBox component
    Resolves #54

--- components/InfoBar.vue
+++ components/InfoBar.vue
@@ -4,19 +4,19 @@ const route = useRoute()
 
 const toggleSettingsPage = () => {
   if (route.path === "/settings") {
-    router.push({name: 'index'})
+    router.push({ name: 'index' })
   } else {
-    router.push({name: 'settings'})
+    router.push({ name: 'settings' })
   }
 }
 </script>
 
 <template>
   <div class="w-full flex gap-2 items-center justify-end">
-    <VersionBox />
 
     <button class="w-max button-small group bg-gray-800 hover:bg-gray-700" @click="toggleSettingsPage()">
-      <span class="material-symbols-rounded !text-[16px] rotate-0 group-hover:rotate-90 transform transition ease-in-out duration-200">settings</span>
+      <span
+        class="material-symbols-rounded !text-[16px] rotate-0 group-hover:rotate-90 transform transition ease-in-out duration-200">settings</span>
     </button>
   </div>
 </template>
